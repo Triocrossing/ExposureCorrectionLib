df21b9270de01bee9a3208f050ed09ea409e0016
triocrossing <classicjazzstore@gmail.com>
2017-07-21 16:53:45 +0200

    Init of Project

---
+++ include/AutoExposure.h
@@ -0,0 +1,11 @@
+//
+// Created by xwang on 7/21/17.
+//
+
+#ifndef AUTOEXPOSURE_AUTOEXPOSURE_H
+#define AUTOEXPOSURE_AUTOEXPOSURE_H
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+#endif //AUTOEXPOSURE_AUTOEXPOSURE_H
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "../include/AutoExposure.h"
+
+int main() {
+  cv::Mat mat;
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
